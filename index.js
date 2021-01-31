64f9f3dd295ff7750b59a558eb51011fe2aaa944
danajackson2 <danajackson2@gmail.com>
2021-01-30 19:42:39 -0800

    woot

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-']+/g, '')
+  }
+  static titleize(string){
+    let arr = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let finalArr = []
+    string.split(' ').forEach(word => {
+      if (arr.includes(word)){
+        finalArr.push(word)
+      } else {
+        finalArr.push(this.capitalize(word))
+      }
+    })
+    return this.capitalize(finalArr.join(' '))
+  }
 }
